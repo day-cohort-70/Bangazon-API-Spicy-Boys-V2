4daa9fdc47136923d231b26a431de2e76d3ca5c4
Danny Hidalgo <dnnysongs@gmail.com>
2024-07-11 11:20:20 -0500

    testing for delete product

--- tests/product.py
+++ tests/product.py
@@ -119,6 +119,14 @@ class ProductTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 3)
 
+    def test_delete_product(self):
+        self.test_create_product()
+
+        url = "/products/1"
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
     # TODO: Delete product
 
     # TODO: Product can be rated. Assert average rating exists.
